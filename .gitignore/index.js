4436a90cae1e190c256c42ff34d7878cda92f573
Zioon-art <66131549+Zioon-art@users.noreply.github.com>
2020-06-09 17:29:43 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,4 +3,4 @@ const client = new Discord.Client();
 
 const fs = require('fs');
 
-client.login("NzExMzIxMzYzMTcxOTAxNDgw.Xt-o6w.xBbc_IfWgNmxLhjl5Ju3j8v-AHk");
+client.login("NzExMzIxMzYzMTcxOTAxNDgw.Xt-qPw.8c7PHwqBaKuR0f4eCde73-wYOMA");
