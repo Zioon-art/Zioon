8465319445f506f53b072c77cea8a9053e9b5879
Zioon-art <66131549+Zioon-art@users.noreply.github.com>
2020-06-09 16:56:49 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const fs = require('fs');
+
+client.login("NzExMzIxMzYzMTcxOTAxNDgw.XtXwJQ.Bh6v5pdSU9SPqlFYt5pHar3y_NI");
